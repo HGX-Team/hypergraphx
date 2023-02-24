15215cf3031a721bb1306b93d71815b04639a64d
Francesco Lotito <lotitoqf@gmail.com>
2023-02-24 14:59:15 +0100

    Add random and scale free models

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import hoinetx as hnx
+from hoinetx.generation.scale_free import scale_free
 from hoinetx.linalg.linalg import *  # fix ugly import
 from hoinetx.representations.projections import bipartite, clique_projection
-from hoinetx.generation.random import random_hypergraph
+from hoinetx.generation.random import *
 
 H = hnx.Hypergraph([(1, 2, 3), (1, 4, 5), (1, 2), (5, 6, 7, 8)])
 print(H)
@@ -30,8 +31,11 @@ print("Weights: {}".format(H.get_weights()))
 
 for edge in H:
     print(edge)
-
+print("OK")
 H = random_hypergraph(10, {2: 5, 3: 2, 4: 1})
 print(H)
 
-print(incidence_matrices_all_orders(H))
+H = scale_free(10, {2: 10, 3: 6, 4: 3}, {2: 2, 3: 1, 4: 1}, correlated=False)
+print(H.degree_sequence(size=2))
+print(H.degree_sequence(size=3))
+print(H.degree_sequence(size=4))
