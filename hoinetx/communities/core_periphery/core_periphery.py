3e6008fe08b621cb7be784356aef4fe242087b75
Francesco Lotito <lotitoqf@gmail.com>
2023-03-08 18:13:11 +0100

    add core periphery, but needs refactor

---
+++ hoinetx/communities/core_periphery/core_periphery.py
@@ -0,0 +1,143 @@
+from loaders import *
+import numpy as np
+import math
+
+LOAD_NODES = 100000
+N_ITER = 10000
+
+
+def count_nodes(d):
+    N = set()
+
+    node2id = {}
+    idx = 0
+
+    new_d = set()
+
+    for e in d:
+        for n in e:
+            if n not in node2id:
+                node2id[n] = idx
+                idx += 1
+            N.add(n)
+
+        tmp = []
+        for n in e:
+            tmp.append(node2id[n])
+        tmp = tuple(sorted(tmp))
+        new_d.add(tmp)
+
+    return len(N), node2id, new_d
+
+
+def sample_params():
+    return np.random.uniform(0, 1), np.random.uniform(0, 1)
+
+
+def transition_function(i, N_nodes, a, b):
+    if i <= math.floor(b * N_nodes):
+        return (i * (1 - a)) / (2 * math.floor(b * N_nodes))
+    else:
+        return ((i - math.floor(b * N_nodes)) * (1 - a)) / (2 * (N_nodes - math.floor(b * N_nodes))) + (1 + a) / 2
+
+
+def aggregation_function(values):
+    return sum(values)
+
+
+def aggregate_local_core_values(nodes, order, local_core_values):
+    values = []
+    for n in nodes:
+        values.append(local_core_values[order[n]])
+    return aggregation_function(values)
+
+
+def get_core_quality(edges, order, local_core_values):
+    R = 0
+    for e in edges:
+        R += aggregate_local_core_values(list(e), order, local_core_values)
+    return R
+
+
+def get_adj(d):
+    adj = {}
+    for e in d:
+        for n in e:
+            if n in adj:
+                adj[n].append(tuple(sorted(list(e))))
+            else:
+                adj[n] = [tuple(sorted(list(e)))]
+
+    return adj
+
+
+# d = load_gene_disease(N_max)
+# d = load_star_dyadic(10)
+d = load_babbuini(LOAD_NODES)
+N_nodes, node2id, d = count_nodes(d)
+adj = get_adj(d)
+id2node = {v: k for k, v in node2id.items()}
+
+cs = {i: 0 for i in range(N_nodes)}
+
+NUM_SWITCH = N_nodes * 10
+
+for n_iter in range(N_ITER):
+    a, b = sample_params()
+    local_core_values = {}
+
+    for i in range(1, N_nodes + 1):
+        local_core_values[i - 1] = transition_function(i, N_nodes, a, b)
+
+    order = {}
+
+    # initial permutation
+    tmp = [i for i in range(N_nodes)]
+    random.shuffle(tmp)
+    for i in range(N_nodes):
+        order[i] = tmp[i]
+
+    R = get_core_quality(d, order, local_core_values)
+
+    # label switching
+    for _ in range(NUM_SWITCH):
+        i, j = random.sample(range(N_nodes), 2)
+
+        new_R = R
+        for e in adj[i]:
+            new_R -= aggregate_local_core_values(list(e), order, local_core_values)
+
+        for e in adj[j]:
+            new_R -= aggregate_local_core_values(list(e), order, local_core_values)
+
+        s_tmp = order[i]
+        order[i] = order[j]
+        order[j] = s_tmp
+
+        for e in adj[i]:
+            new_R += aggregate_local_core_values(list(e), order, local_core_values)
+
+        for e in adj[j]:
+            new_R += aggregate_local_core_values(list(e), order, local_core_values)
+
+        if new_R < R:
+            s_tmp = order[i]
+            order[i] = order[j]
+            order[j] = s_tmp
+        else:
+            R = new_R
+
+    for node in range(N_nodes):
+        cs[node] = cs[node] + local_core_values[order[node]] * R
+
+    if n_iter % 1000 == 0:
+        print("{} of {} iter".format(n_iter, N_ITER))
+
+max_node = max(cs, key=cs.get)
+Z = 1 / cs[max_node]
+
+for node in range(N_nodes):
+    cs[node] = Z * cs[node]
+
+for node in range(N_nodes):
+    print(id2node[node], cs[node])
