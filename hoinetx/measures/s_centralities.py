537745cddef7f5cb32c77dba1a4180ff7b21645a
Francesco Lotito <lotitoqf@gmail.com>
2023-03-08 15:43:02 +0100

    add s-betweenness and s-closeness centrality

---
+++ hoinetx/measures/s_centralities.py
@@ -0,0 +1,39 @@
+import networkx as nx
+
+from hoinetx.core.hypergraph import Hypergraph
+from hoinetx.representations.projections import line_graph
+
+
+def s_betweenness(H: Hypergraph, s=1):
+    """
+    Compute the betweenness centrality for each edge in the hypergraph.
+    Parameters
+    ----------
+    H
+    s
+
+    Returns
+    -------
+
+    """
+
+    lg, id_to_edge = line_graph(H, s=s)
+    b = nx.betweenness_centrality(lg)
+    return {id_to_edge[k]: v for k, v in b.items()}
+
+
+def s_closeness(H: Hypergraph, s=1):
+    """
+    Compute the closeness centrality for each edge in the hypergraph.
+    Parameters
+    ----------
+    H
+    s
+
+    Returns
+    -------
+
+    """
+    lg, id_to_edge = line_graph(H, s=s)
+    c = nx.closeness_centrality(lg)
+    return {id_to_edge[k]: v for k, v in c.items()}
