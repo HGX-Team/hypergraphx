94f47d1ee4a7dc532117ea28b834428ad269fce4
Francesco Lotito <lotitoqf@gmail.com>
2023-03-09 12:28:34 +0100

    fix pickle save e load

--- hoinetx/readwrite/load.py
+++ hoinetx/readwrite/load.py
@@ -1,6 +1,19 @@
 import pickle
 
 
-def load_pickle(file_name):
+def _load_pickle(file_name):
     with open("{}".format(file_name), "rb") as f:
         return pickle.load(f)
+
+
+def load_hypergraph(file_name, file_type="pickle"):
+    if file_type == "pickle":
+        _load_pickle(file_name)
+    elif file_type == "json":
+        pass
+    elif file_type == "text":
+        pass
+    elif file_type == "csv":
+        pass
+    else:
+        raise ValueError("Invalid file type.")
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from hoinetx.generation.scale_free import scale_free
 from hoinetx.linalg import *
 from hoinetx.representations.projections import bipartite, clique_projection
 from hoinetx.generation.random import *
+from hoinetx.readwrite.save import save_hypergraph, load_hypergraph
 
-H = hnx.Hypergraph([(1, 3), (1, 4), (1, 2), (5, 6, 7, 8), (1, 2, 3)])
-print(laplacian_matrix_by_order(H, 1))
+#H = hnx.Hypergraph([(1, 3), (1, 4), (1, 2), (5, 6, 7, 8), (1, 2, 3)])
+#save(H, "test.hnx", "pickle")
+
+H = load_hypergraph("test.hnx", "pickle")
+print(H)
