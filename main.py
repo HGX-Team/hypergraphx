5e8444292d37f85fb19fec690598ed1df1569530
Francesco Lotito <lotitoqf@gmail.com>
2023-03-17 09:57:11 +0100

    add notebook tutorial viz

--- main.py
+++ main.py
@@ -11,4 +11,5 @@ import sklearn
 
 #H = hnx.Hypergraph([(1, 2, 3), (1, 3, 4, 5), (1, 2), (3, 4)])
 #draw_pie(H)
-H = load_hypergraph("test_data/hospital/hospital.json", "json")
+H = load_hypergraph("test_data/workspace/workspace.json", "json")
+print(H.get_meta(875))
