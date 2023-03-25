d1dabe8ffd8555661124d740fa83be42ec6c669d
Martina Contisciani <martina.contisciani@gmail.com>
2023-03-25 15:05:19 +0100

    update draw_communities

--- hnx/viz/__init__.py
+++ hnx/viz/__init__.py
@@ -1,4 +1,4 @@
-from .draw_hypergraph import draw_HG
+from .draw_hypergraph import draw_hypergraph
 from .draw_projections import draw_clique
 from .draw_projections import draw_bipartite
 from .draw_communities import draw_communities
