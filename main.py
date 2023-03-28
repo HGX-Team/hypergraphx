0296abdef8d8cc62106e482adf49116e866c14aa
Francesco Lotito <lotitoqf@gmail.com>
2023-03-28 04:45:38 +0200

    fix

--- main.py
+++
@@ -1,8 +0,0 @@
-from hypergraphx.core import Hypergraph
-
-H = Hypergraph()
-H.add_edge([1, 2, 3, 4])
-H.add_edge([1, 2, 3, 5])
-H.add_edge([1, 2, 3, 6])
-H.add_edge([1, 3, 7])
-print(H.is_connected())
