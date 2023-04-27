5297ff3d44465408db71f853c9edef6179e86473
Francesco Lotito <lotitoqf@gmail.com>
2023-04-27 17:44:10 +0200

    Update __init__.py

--- hypergraphx/__init__.py
+++ hypergraphx/__init__.py
@@ -1,1 +1,32 @@
+import pkg_resources
+
+from . import (
+    core,
+    communities,
+    dynamics,
+    generation,
+    filters,
+    linalg,
+    measures,
+    motifs,
+    readwrite,
+    representations,
+    utils,
+    viz,
+)
+
 from .core import *
+from .communities import *
+from .dynamics import *
+from .generation import *
+from .filters import *
+from .linalg import *
+from .measures import *
+from .motifs import *
+from .readwrite import *
+from .representations import *
+from .utils import *
+from .viz import *
+
+__version__ = pkg_resources.get_distribution("hypergraphx").version
+
