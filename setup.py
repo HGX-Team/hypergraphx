e9333b2109ce7789c99ca08f9258ec1b329da1fc
Francesco Lotito <lotitoqf@gmail.com>
2023-05-17 10:20:17 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,51 @@
-from distutils.core import setup
+import io
+import os
+from setuptools import find_packages, setup
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("project_name", "VERSION")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
+
 setup(
   name = 'hypergraphx',         
   packages = ['hypergraphx', 'hypergraphx.core'], 
   version = '1.0',      
   license='BSD-3-Clause license',        
   description = 'HGX is a multi-purpose, open-source Python library for higher-order network analysis',   
+  long_description=read("README.md"),
+  long_description_content_type="text/markdown",
   author = 'HGX-Team',              
   author_email = 'lotitoqf@gmail.com',      
   url = 'https://github.com/HGX-Team/hypergraphx',   
   download_url = 'https://github.com/HGX-Team/hypergraphx/archive/refs/tags/1.1.1.tar.gz',    
   keywords = ['hypergraphs', 'networks'], 
-  install_requires=[
-          'numpy',
-          'scipy',
-          'networkx',
-          'rpy2',
-          'pandas',            
-          'seaborn',
-          'matplotlib',
-          'pytest',
-          'scikit-learn',
-      ],
+  packages=find_packages(exclude=["tests", ".github"]),
+  install_requires=read_requirements("requirements.txt"),
+  entry_points={
+      "console_scripts": ["project_name = project_name.__main__:main"]
+  },
+  extras_require={"test": read_requirements("requirements-test.txt")},
+  
   classifiers=[
     'Development Status :: 3 - Alpha',      
     'Intended Audience :: Developers',      
