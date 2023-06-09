08117b0ec14792cf8b10a801570b0b5715ad301b
Francesco Lotito <lotitoqf@gmail.com>
2023-06-09 19:30:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,22 @@ import io
 import os
 from setuptools import find_packages, setup
 
+def read(*paths, **kwargs):
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
 setup(
   name = 'hypergraphx',         
-  version = '1.4.1',      
+  version = '1.4.2',      
   license='BSD-3-Clause license',        
-  description = 'HGX is a multi-purpose, open-source Python library for higher-order network analysis',   
-  long_description= 'HGX is a multi-purpose, open-source Python library for higher-order network analysis',
+  description = 'HGX is a multi-purpose, open-source Python library for higher-order network analysis',
+  long_description=read("README.md"),
+  long_description_content_type="text/markdown",
   author = 'HGX-Team',              
   author_email = 'lotitoqf@gmail.com',      
   url = 'https://github.com/HGX-Team/hypergraphx',   
