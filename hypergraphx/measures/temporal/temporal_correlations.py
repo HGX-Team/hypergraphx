1b9d8eecb6269a515da013c172f42c1ed9bb571e
Luca Gallo <GalloL@ceu.edu>
2023-10-10 14:40:01 +0200

    Further debug

--- hypergraphx/measures/temporal/temporal_correlations.py
+++ hypergraphx/measures/temporal/temporal_correlations.py
@@ -27,7 +27,7 @@ def intra_order_correlation_matrix_by_order(
     The intra-order correlation matrix of order d at time lag tau, as a sparse matrix.
     """
     T = max(temporal_hypergraph.keys())
-    temporal_adjacency_matrix,  = temporal_adjacency_matrix_by_order(
+    temporal_adjacency_matrix, _ = temporal_adjacency_matrix_by_order(
         temporal_hypergraph, order
     )
     annealed_adjacency_mtx = annealed_adjacency_matrix(temporal_adjacency_matrix)
