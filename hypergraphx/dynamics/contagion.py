e86ead8bb750ceb90d5d301c17a632cf7f51fa88
Luca Gallo <119935568+lgajo@users.noreply.github.com>
2024-03-04 18:16:22 +0100

    Debug

--- hypergraphx/dynamics/contagion.py
+++ hypergraphx/dynamics/contagion.py
@@ -82,7 +82,7 @@ def simplicial_contagion(hypergraph, I_0, T, beta, beta_D, mu):
         #I_old = np.copy(I_new)
         I_old = I_new.copy()
         #Infected = np.sum(I_new)
-        Infected = sum(I_new.values()
+        Infected = sum(I_new.values())
         numberInf[t] = Infected
         t = t+1
     
