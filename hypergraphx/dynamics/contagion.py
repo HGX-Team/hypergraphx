589738a3ae7d2edd5a3d2d15f2cd832310cc9930
Luca Gallo <119935568+lgajo@users.noreply.github.com>
2024-03-04 18:05:38 +0100

    Faster contagion

--- hypergraphx/dynamics/contagion.py
+++ hypergraphx/dynamics/contagion.py
@@ -38,45 +38,51 @@ def simplicial_contagion(hypergraph, I_0, T, beta, beta_D, mu):
     """
     
     numberInf = np.linspace(0, 0, T)
-    Infected = np.sum(I_0)
+    #Infected = np.sum(I_0)
+    Infected = np.sum(I_0.values())
     numberInf[0] = Infected
     N = len(I_0)
     nodes = hypergraph.get_nodes()
     mapping = hypergraph.get_mapping()
-    I_old = np.copy(I_0)
+    #I_old = np.copy(I_0)
+    I_old = I_0.copy()
     t = 1
 
-    while np.sum(Infected)>0 and t<T:
-        I_new = np.copy(I_old)
+    while Infected>0 and t<T:
+        #I_new = np.copy(I_old)
+        I_new = I_old.copy()
 
         # We run over the nodes
         for node in nodes:
-            n = map_node(mapping, node)
+            #n = map_node(mapping, node)
             # if the node is susceptible, we run the infection process
-            if I_old[n] == 0: 
+            if I_old[node] == 0: 
                 # we first run the two-body infections
                 neighbors = hypergraph.get_neighbors(node, order=1)
                 for neigh in neighbors:
-                    m = map_node(mapping, neigh)
-                    if I_old[m] == 1 and np.random.random() < beta:
-                        I_new[n] = 1
+                    #m = map_node(mapping, neigh)
+                    if I_old[neigh] == 1 and np.random.random() < beta:
+                        I_new[node] = 1
                         break # if the susceptile node gets infected, we stop iterating over its neighbors
-                if I_new[n] == 1: continue # if the susceptile node is already infected, we don't run the three-body processes
+                if I_new[node] == 1: continue # if the susceptile node is already infected, we don't run the three-body processes
                 # we run the three-body infections
                 triplets = hypergraph.get_incident_edges(node, order=2)
                 for triplet in triplets:
                     neighbors = list(triplet)
                     neighbors.remove(node)
-                    m1,m2 = tuple(map_nodes(mapping,neighbors))
-                    if I_old[m1] == 1 and I_old[m2] == 1 and np.random.random() < beta_D:
-                        I_new[n] = 1
+                    neigh1, neigh2 = tuple(neighbors)
+                    #m1,m2 = tuple(map_nodes(mapping,neighbors))
+                    if I_old[neigh1] == 1 and I_old[neigh2] == 1 and np.random.random() < beta_D:
+                        I_new[node] = 1
                         break # if the susceptile node gets infected, we stop iterating over the triplets
             # if the node is infected, we run the recovery process
             elif np.random.random() < mu:
-                I_new[n] = 0
+                I_new[node] = 0
         
-        I_old = np.copy(I_new)
-        Infected =  np.sum(I_new)
+        #I_old = np.copy(I_new)
+        I_old = I_new.copy()
+        #Infected = np.sum(I_new)
+        Infected = np.sum(I_new.values())
         numberInf[t] = Infected
         t = t+1
     
