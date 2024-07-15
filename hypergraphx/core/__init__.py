b64a631e16bf467fd2519ad2c9a906057d4da94d
Francesco Lotito <lotitoqf@gmail.com>
2024-07-15 15:45:40 +0200

    proposal for HIF read/write

--- hypergraphx/core/__init__.py
+++ hypergraphx/core/__init__.py
@@ -1,1 +1,3 @@
 from hypergraphx.core.hypergraph import Hypergraph
+from hypergraphx.core.multiplex_hypergraph import MultiplexHypergraph
+from hypergraphx.core.temporal_hypergraph import TemporalHypergraph
