9284969d2d4ad6656c530ad3ca08ebd6ca730c0d
Francesco Lotito <lotitoqf@gmail.com>
2025-03-25 11:57:20 +0100

    New version

--- hypergraphx/__init__.py
+++ hypergraphx/__init__.py
@@ -4,4 +4,8 @@ from hypergraphx.core.multiplex_hypergraph import MultiplexHypergraph
 from hypergraphx.core.temporal_hypergraph import TemporalHypergraph
 from . import readwrite
 
-__version__ = "1.7.5"
+import sys
+MIN_PYTHON_VERSION = (3, 10)
+assert sys.version_info >= MIN_PYTHON_VERSION, f"requires Python {'.'.join([str(n) for n in MIN_PYTHON_VERSION])} or newer"
+
+__version__ = "1.7.6"
