c617d6293b425fd94634968775e14c0ff4ee6fc6
Francesco Lotito <lotitoqf@gmail.com>
2025-12-18 14:57:51 +0100

    Update version

--- hypergraphx/__init__.py
+++ hypergraphx/__init__.py
@@ -11,4 +11,4 @@ assert (
     sys.version_info >= MIN_PYTHON_VERSION
 ), f"requires Python {'.'.join([str(n) for n in MIN_PYTHON_VERSION])} or newer"
 
-__version__ = "1.7.8"
+__version__ = "1.7.9"
