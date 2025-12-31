b756a7a73bbcd0c2e73462f136db837b8e079d76
Francesco Lotito <lotitoqf@gmail.com>
2025-12-31 16:07:27 +0100

    Migrate to pyproject, clean legacy setup

--- docs/installation.rst
+++ docs/installation.rst
@@ -25,7 +25,9 @@ If you only need core functionality, the base install is enough. For docs or not
 .. code:: bash
 
    pip install -r requirements/docs.txt
+   pip install .[docs]
 
 Check the repository ``requirements/runtime.txt`` for the required runtime dependencies.
 Development and documentation dependencies live in ``requirements/dev.txt`` and
 ``requirements/docs.txt`` respectively.
+You can also install optional extras with ``pip install .[dev]`` or ``pip install .[docs]``.
--- hypergraphx/__init__.py
+++ hypergraphx/__init__.py
@@ -20,6 +20,7 @@ from . import readwrite
 
 import logging
 import sys
+from importlib.metadata import PackageNotFoundError, version
 
 MIN_PYTHON_VERSION = (3, 10)
 if sys.version_info < MIN_PYTHON_VERSION:
@@ -27,7 +28,10 @@ if sys.version_info < MIN_PYTHON_VERSION:
         f"requires Python {'.'.join([str(n) for n in MIN_PYTHON_VERSION])} or newer"
     )
 
-__version__ = "1.7.9"
+try:
+    __version__ = version("hypergraphx")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
 
 logging.getLogger("hypergraphx").addHandler(logging.NullHandler())
 
